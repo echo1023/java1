d716453115977fc5575050f6a8af0e5d2a8a11af
Your Name <euy.cho@gmail.com>
2018-12-13 23:47:17 +0900

    first commit

---
+++ src/Operators.java
@@ -0,0 +1,12 @@
+public class Operators {
+    public void calculate() {
+        System.out.println(1+2);
+        System.out.println(3-2);
+        System.out.println(3*4);
+        System.out.println(6/2);
+
+        // special case
+        System.out.println(5/2);
+        System.out.println(5%2);
+    }
+}
